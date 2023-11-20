4f8ae9cc7735557474b6a744393b90bf138e8898
muhammadimam155 <imamaliatari@gmail.com>
2023-11-21 01:48:12 +0500

    js pattern

---
+++ app.js
@@ -0,0 +1,143 @@
+document.write(" <h1>Q1 </h1>")
+for (var i = 1 ; i<=5 ; i++ ){
+   
+    for (var j = 1 ; j<=i ; j++){
+     document.write("*" )   
+    }
+    document.write("<br/>")
+}
+
+
+
+
+
+document.write(" <h1>Q2 </h1>")
+for (var i = 1 ; i<=5 ; i++ ){
+    for(k=1 ; k<=5-i ; k++ ){
+        document.write("&nbsp;&nbsp;")
+    }
+   
+    for (var j = 1 ; j<=i ; j++){
+     document.write("*" )   
+    }
+    document.write("<br/>")
+}
+
+
+
+
+
+document.write(" <h1>Q3 </h1>")
+for (var i = 1 ; i<=5 ; i++ ){
+    for(k=1 ; k<=5-i ; k++ ){
+        document.write("&nbsp;&nbsp;")
+    }
+   
+    for (var j = 1 ; j<=i ; j++){
+     document.write("*" )   
+    }
+    for (var l = 2 ; l<=i ; l++ ){
+        document.write("*")
+    }
+    document.write("<br/>")
+}
+
+
+
+
+
+document.write(" <h1>Q4 </h1>")
+for (var i = 4 ; i<=5 ; i++ ){
+    for(k=1 ; k<=5-i ; k++ ){
+        document.write("&nbsp;&nbsp;")
+    }
+   
+    for (var j = 1 ; j<=i ; j++){
+     document.write("*" )   
+    }
+    for (var l = 2 ; l<=i ; l++ ){
+        document.write("*")
+    }
+    document.write("<br/>")
+}
+
+
+
+
+
+document.write(" <h1> Q5 </h1>")
+for (var i = 1 ; i<=5 ; i++ ){
+    for(k=1 ; k<=5-i ; k++ ){
+        document.write("&nbsp;&nbsp;")
+    }
+   
+    for (var j = 1 ; j<=i ; j++){
+     document.write(i)   
+    }
+    document.write("<br/>")
+}
+
+
+
+
+document.write(" <h1>Q6 </h1>")
+for (var i = 1 ; i<=5 ; i++ ){
+    for(k=1 ; k<=5-i ; k++ ){
+        document.write("&nbsp;&nbsp;")
+    }
+   
+    for (var j = 1 ; j<=i ; j++){
+     document.write("*" )   
+    }
+    for (var l = 2 ; l<=i ; l++ ){
+        document.write("*")
+    }
+    document.write("<br/>")
+}
+for (var i=4 ;i>=1 ; i--){
+    for (var j = 1 ;j<=2; j++){
+        document.write("&nbsp;");
+     }
+    for(var k = 1 ; k<=i ; k++){
+        document.write("*");
+    }
+     for(var l = 1 ; l<=i-1 ; l++){
+        document.write("*")
+    }
+    document.write("<br/>")
+}
+
+
+
+
+
+document.write(" <h1>Q7 </h1>")
+for (var i = 1 ; i<=5 ; i++ ){
+    for(k=1 ; k<=5-i ; k++ ){
+        document.write("&nbsp;&nbsp;")
+    }
+   
+    for (var j = 1 ; j<=i ; j++){
+     document.write(i)   
+    }
+    for (var l = 2 ; l<=i ; l++ ){
+        document.write(i)
+    }
+    document.write("<br/>")
+}
+for (var i=4 ;i>=1 ; i--){
+    for (var j = 1 ;j<=2; j++){
+        document.write("&nbsp;");
+     }
+    for(var k = 1 ; k<=i ; k++){
+        document.write(i);
+    }
+     for(var l = 1 ; l<=i-1 ; l++){
+        document.write(i)
+    }
+    document.write("<br/>")
+}
+
+
+
+
